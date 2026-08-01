4ae38c6167a49fd53c692c0403ba4f9214d63a5d
vishalmaurya87 <mauryavishal4604@gmail.com>
2026-08-02 00:04:10 +0530

    Time: 49 ms (57.26%) | Memory: 50.4 MB (28.48%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -0,0 +1,34 @@
+// import java.util.HashMap;
+// import java.util.Map;
+
+class Solution {
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int left = 0;
+        int count = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int right = 0; right < nums.length; right++) {
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+
+            while (freq.size() > k) {
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+
+                if (freq.get(nums[left]) == 0) {
+                    freq.remove(nums[left]);
+                }
+
+                left++;
+            }
+
+            // Number of valid subarrays ending at 'right'
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
