55664c457ea8a7a63505f989af9186e8130f1bd9
vishalmaurya87 <mauryavishal4604@gmail.com>
2026-08-28 10:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (75.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums2.size();
-        vector<int> nge(n, -1);
+        vector<int> res(nums1.size());
         stack<int> st;
-
-        // Traverse nums2 from right
-        for(int i = n - 1; i >= 0; i--) {
-
-            while(!st.empty() && st.top() <= nums2[i]) {
+        unordered_map<int,int> map;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
                 st.pop();
             }
-
-            if(!st.empty()) {
-                nge[i] = st.top();
+            if(st.empty()){
+                map[nums2[i]] = -1;
             }
-
+            else{
+                map[nums2[i]] = st.top();
+            }
             st.push(nums2[i]);
+            
         }
-
-        vector<int> ans;
-
-        // Find nums1 elements in nums2
-        for(int i = 0; i < nums1.size(); i++) {
-
-            for(int j = 0; j < nums2.size(); j++) {
-
-                if(nums1[i] == nums2[j]) {
-                    ans.push_back(nge[j]);
-                    break;
-                }
-            }
+      
+        for(int i=0;i<nums1.size();i++){
+            res[i] =map[nums1[i]];
         }
-
-        return ans;
+        return res;
     }
 };
