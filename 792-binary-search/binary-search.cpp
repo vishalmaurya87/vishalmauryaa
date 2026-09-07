cb3789ba1aaedd31a32f8c1b3984bc2dfbf8ca20
vishalmaurya87 <mauryavishal4604@gmail.com>
2026-09-07 17:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.18%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int guess= (low+high)/2;
+            if(nums[guess] == target) return guess;
+            else if(nums[guess] < target){
+                low= guess+1;
+            }
+            else{
+                high = guess-1;
+            }
+        }return -1;
+    }
+};
