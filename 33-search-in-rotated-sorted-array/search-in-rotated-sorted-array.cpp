8df9853fe4e55896cff8b9ac3636f13d20930f98
vishalmaurya87 <mauryavishal4604@gmail.com>
2026-09-12 20:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (69.60%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n =nums.size();
-        int low =0;
+        int n = nums.size();
+        int low = 0;
         int high = n-1;
-        int guess = -1;
         while(low<=high){
-            guess = (low+high)/2;
+            int guess = (low + high)/2;
             if(nums[guess] == target){
                 return guess;
             }
@@ -15,31 +14,29 @@ public:
                 if(nums[guess] < target){
                     low = guess+1;
                 }
-                else{
+                else{//nums[guess] > target
                     if(nums[0] > target){
-                        low= guess+1;
+                        low = guess +1;
                     }
                     else{
                         high = guess -1;
                     }
                 }
             }
-            //part 2
+            //part2
             else{
                 if(nums[guess] > target){
-                high = guess-1;
+                    high = guess-1;
                 }
-                else{
-                    if(nums[guess] > target){
-                        high =guess-1;
+                else{// nums[guess] < target
+                    if(nums[n-1] < target){
+                        high = guess-1;
                     }
                     else{
-                        if(nums[n-1] < target)
-                        high = guess-1;
-                        else low = guess+1;
+                        low = guess +1;
                     }
                 }
             }
-        }return -1;
+        } return -1;
     }
 };
